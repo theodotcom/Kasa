b5ad9fe044d050c88d37ef926740b486e3f50a37
theodotcom <61735898+theodotcom@users.noreply.github.com>
2022-09-21 16:32:12 +0200

    Listing form

---
+++ src/pages/ListingForm.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function ListingForm (){
+return (
+<div className="listingform-container">
+    <Header />
+    <Footer />
+</div>
+)
+}
+
+export default ListingForm
