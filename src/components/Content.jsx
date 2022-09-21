7e026cafe04d4e129b65f1191b61b687cc759719
theodotcom <61735898+theodotcom@users.noreply.github.com>
2022-09-21 18:26:19 +0200

    first commit Listing form

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+// dropdown description & Equipements + Tags + Rates
+//
+
+import { useParams } from 'react-router-dom'
+import data from '../datas/listings.json'
+
+
+//  const { id } = useParams()
+
+function Content (){
+    const {id} = useParams()
+}
