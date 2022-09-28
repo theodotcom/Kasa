b115c84bce1009b8b0e3c73a1146a6b24e632625
theodotcom <61735898+theodotcom@users.noreply.github.com>
2022-09-28 16:27:47 +0200

    edit

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -1,6 +1,6 @@
 import { FaStar } from 'react-icons/fa'
 
-function Star({ filled }) {
+const Star = ({ filled }) => {
   return <FaStar color={filled ? 'orange' : 'lightgray'} />
 }
 export default Star
